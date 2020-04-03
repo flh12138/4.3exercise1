33c0f3775aa36b93b18c20248a1c55dc3b96e5eb
FLH0202 <FLH0202@163.com>
2020-04-03 14:18:42 +0800

    kl

---
+++ 4.3exercise1/main.cpp
@@ -0,0 +1,27 @@
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+		cv::Mat frame = imread("E:\\PIC\\lena.jpg");
+		cv::Mat frame1 = imread("E:\\PIC\\lena.jpg",0);
+		cv::Mat SobelMat1;
+		cv::Mat SobelMat2;
+		cv::Mat outputMat1;
+		cv::Mat outputMat2;
+
+		Sobel(frame, SobelMat1, CV_16SC1, 1, 0, 3);
+		//convertScaleAbs(SobelMat1, L_SobelMat1);
+		Sobel(frame, SobelMat2, CV_16SC1, 0, 1, 3);
+		//convertScaleAbs(SobelMat2, L_SobelMat2);
+		Canny(SobelMat1, SobelMat2, outputMat2,20,60);
+
+		Canny(frame1,outputMat1,20,60);
+
+		imshow("src1",outputMat1 );
+		imshow("src2", outputMat2);
+		imshow("frame", frame);
+		waitKey();
+}
